b15521592439deb18071325dba4cf1ee8a1db95d
activekang <33951519+activekang@users.noreply.github.com>
2022-05-30 17:47:40 +0900

    Vuetify&Router 실습

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+      <span>Vuetify</span>
+      <v-btn><router-link to="/">Home</router-link></v-btn>
+      <v-btn><router-link to="/about">About</router-link></v-btn>
+      <v-btn><router-link to="/port">Portfolio</router-link></v-btn>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+   // HelloWorld
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
---
+++ views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+   <h1>homepage main</h1>
+  <!-- <hello-world /> -->
+</template>
+
+<script>
+ // import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    
+    components: {
+     // HelloWorld,
+    },
+  }
+</script>
